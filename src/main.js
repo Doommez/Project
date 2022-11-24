9d69b4c4bc8ced11cda4ee8e7f07b80fa48aac07
Doommez <jenyaz862@gmail.com>
2022-11-24 19:39:35 +0300

    feat!: install vuetify 3
    install Material Design Icons, css-loader

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue';
 import './style.css';
 import { createPinia } from 'pinia';
+import vuetify from './plugins/vuetify.js';
 import App from './App.vue';
 import router from './router';
 
 const pinia = createPinia();
 const app = createApp(App);
-
+app.use(vuetify);
 app.use(pinia);
 app.use(router);
 app.mount('#app');
